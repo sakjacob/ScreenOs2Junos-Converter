7b0a1a0cde8be9ddcef053ded9e91ff0a3c0b7d3
Sak <sakjacob@msu.edu>
2020-04-09 13:00:00 -0400

    Added GUI to config-comparor.py. Now automatically compares example to tool AND tool to example and outputs both results to a folder the user selects.

--- config-comparor.py
+++ config-comparor.py
@@ -5,15 +5,14 @@ To Do
 """
 
 import math
+from tkinter import filedialog
+from tkinter import *
+import tkinter as tk
 
 
-def main():
-    userChoice = input("Type 1 to compare orginal to tool, 2 to compare tool to original: ")
-    tool_config = "C:\\Users\\sakjacob\\Desktop\\S2J\\bcc\\bcc-orig-srx_tool.config"
-    fp_tool = open(tool_config,"r")
-    orig_config = "C:\\Users\\sakjacob\\Desktop\\S2J\\bcc\\bcc-srx.config"
-    print(orig_config)
-    fp_orig = open(orig_config,"r")
+def main(example_filename, tool_filename, save_folder):
+    fp_tool = open(tool_filename,"r") # filename for tool config
+    fp_orig = open(example_filename,"r") # filename for example config
 
     orig_lines = fp_orig.readlines()
     orig_set = set(orig_lines)
@@ -21,37 +20,118 @@ def main():
     tool_set = set(tool_lines)
 
 
-    if int(userChoice) == 2:
-        print("Checking if lines from tool exist in the orginal.")
-        log = "C:\\Users\\sakjacob\\Desktop\\S2J\\bcc\\lines-not-in-orig.txt"
-        fp_log = open(log, "w")
-        line_num = 1
-        for line in tool_lines:
-            if line not in orig_set:
-                print("\nline ",line_num, "is not in the orig set")
-                print(line)
-                fp_log.write(line)
-            line_num += 1
-        fp_log.close()
-    elif int(userChoice) == 1:
-        print("Checking if lines from orginal exist in the tool.")
-        log = "C:\\Users\\sakjacob\\Desktop\\S2J\\bcc\\lines-missing-in-tool.txt"
-        fp_log = open(log, "w")
-        line_num = 1
-        for line in orig_lines:
-            if line not in tool_set:
-                print("\nline ",line_num, "is not in the tool set")
-                print(line)
-                fp_log.write(line)
-            line_num += 1
-        fp_log.close()
+    print("Checking if lines from tool exist in the orginal.")
+    log = save_folder + "\\lines-not-in-orig.txt"
+    fp_log = open(log, "w")
+    line_num = 1
+    for line in tool_lines:
+        if line not in orig_set:
+            print("\nline ",line_num, "is not in the orig set")
+            print(line)
+            fp_log.write(line)
+        line_num += 1
+    fp_log.close()
+    print("Checking if lines from orginal exist in the tool.")
+    log = save_folder + "\\lines-missing-in-tool.txt"
+    fp_log = open(log, "w")
+    line_num = 1
+    for line in orig_lines:
+        if line not in tool_set:
+            print("\nline ",line_num, "is not in the tool set")
+            print(line)
+            fp_log.write(line)
+        line_num += 1
+    fp_log.close()
 
     fp_orig.close()
     fp_tool.close()
     print("Done")
 
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+        self.e_path = "" # filename for example config 
+        self.dst_path = "" # path for destination config
+        self.e_bool = False # update to true once eConfig filename acquired
+        self.dst_bool = False # update to true once destination path acquired
+        self.t_path = "" # filename for tool config
+        self.t_bool = False # update to true once tool config filename acquired
+
+    def create_widgets(self):
+        self.eConfig = tk.Button(self) # example config
+        self.eConfig["text"] = "Select eConfig config"
+        self.eConfig["command"] = self.Update_Example
+        self.eConfig.grid(column=0, row=0)
+        self.e_lbl = Label(self, text="Nothing selected")
+        self.e_lbl.grid(column=1, row = 0)
+
+        self.tConfig = tk.Button(self) # tool config
+        self.tConfig["text"] = "Select tConfig config"
+        self.tConfig["command"] = self.Update_Tool
+        self.tConfig.grid(column=0, row=1)
+        self.t_lbl = Label(self, text="Nothing selected")
+        self.t_lbl.grid(column=1, row = 1)
+
+        self.dst = tk.Button(self)
+        self.dst["text"] = "Select Directory to save \n new config and other outputs"
+        self.dst["command"] = self.Update_dst
+        self.dst.grid(column = 0, row = 2)
+        self.dst_lbl = Label(self, text="Nothing selected")
+        self.dst_lbl.grid(column=1, row = 2)
 
 
+        
 
-if __name__ == "__main__":
-    main()
+        self.run = tk.Button(self)
+        self.run["text"] = "Start conversion"
+        self.run["command"] = self.Run
+        self.run.grid(row=3)
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.grid(row=4)
+
+
+    def Update_Example(self):
+        temp = tk.filedialog.askopenfilename()
+        if temp is not None:
+            self.e_bool = True
+            self.e_path = temp
+            self.e_lbl["text"] = temp
+            print(temp)
+
+    def Update_Tool(self):
+        temp = tk.filedialog.askopenfilename()
+        if temp is not None:
+            self.t_bool = True
+            self.t_path = temp
+            self.t_lbl["text"] = temp
+            print(temp)
+
+    def Update_dst(self):
+        temp = tk.filedialog.askdirectory()
+        if temp != "":
+            self.dst_bool = True
+            self.dst_path = temp
+            self.dst_lbl["text"] = temp
+            print(temp)
+
+    def Run(self):
+        if self.e_bool and self.t_bool and self.dst_bool:
+            print("Successful run")
+            main(self.e_path,self.t_path,self.dst_path)
+        else:
+            print("Nope!")
+
+root = tk.Tk()
+root.geometry('450x200')
+root.title("Junos Config Comparor")
+app = Application(master=root)
+app.mainloop()
+
+# Orig2Edit.Convert(self.e_path)
+# edit_FileName = self.e_path[:self.e_path.find(".txt")] + "-edit_tool.txt" 
+# Edit2Config.Convert(edit_FileName)
