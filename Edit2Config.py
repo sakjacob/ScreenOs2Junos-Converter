c45ab97d4fbf34d150d49136f603ed93c19298b7
Sak <sakjacob@msu.edu>
2020-12-23 16:42:31 -0500

    updated syntax of deactivated policies

--- Edit2Config.py
+++ Edit2Config.py
@@ -368,7 +368,7 @@ def Convert(edit_Filename, save_directory, tkinter_object):
                 output = beginning + " match application " + app + '\n'
             fp_config.write(output)
         if IterPolicy.mDisabled:
-            fp_config.write("deactivate security policies from-zone " + IterPolicy.mFromZone + " to-zone " + IterPolicy.mToZone + " policy " + str(IterPolicy.mID) +'\n')
+            fp_config.write("deactivate logical-systems " + lSystem + " security policies from-zone " + IterPolicy.mFromZone + " to-zone " + IterPolicy.mToZone + " policy " + str(IterPolicy.mID) +'\n')
         for action in IterPolicy.mAction:
             if action == "permit" or action == "deny" or action == "count" or action == "reject":
                 fp_config.write(beginning + " then " + action + '\n')
